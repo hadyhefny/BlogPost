3ac9796273c67167e6f2ff0cf2512d5508f72faf
hady <hhefny@users.noreply.github.com>
2020-02-04 15:10:18 +0200

    MVI Generics setup
    setup of generic classe:
    StateResourse
    DataState
    BaseViewModel
    AbsentLiveData(helper class)

---
+++ app/src/main/java/com/hefny/hady/animalfeed/ui/BaseViewModel.kt
@@ -0,0 +1,31 @@
+package com.hefny.hady.animalfeed.ui
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+
+abstract class BaseViewModel<StateEvent, ViewState> : ViewModel() {
+    val TAG = "AppDebug"
+    protected val _stateEvent: MutableLiveData<StateEvent> = MutableLiveData()
+    protected val _viewState: MutableLiveData<ViewState> = MutableLiveData()
+    val viewState: LiveData<ViewState>
+        get() = _viewState
+    val dataState: LiveData<DataState<ViewState>> = Transformations
+        .switchMap(_stateEvent) { stateEvent ->
+            handleStateEvent(stateEvent)
+        }
+
+    fun setStateEvent(event: StateEvent) {
+        _stateEvent.value = event
+    }
+
+    fun getCurrentViewStateOrNew(): ViewState {
+        return viewState.value?.let {
+            it
+        } ?: initNewViewState()
+    }
+
+    abstract fun handleStateEvent(stateEvent: StateEvent): LiveData<DataState<ViewState>>
+    abstract fun initNewViewState(): ViewState
+}
