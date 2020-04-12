7bce118f6a3eb8064a824fffbc11a147885a0941
hady <hhefny@users.noreply.github.com>
2020-04-12 13:36:52 +0200

    RecyclerView Adapter, DiffUtil and ListUpdateCallback

--- app/src/main/java/com/hefny/hady/blogpost/ui/main/blog/BlogFragment.kt
+++ app/src/main/java/com/hefny/hady/blogpost/ui/main/blog/BlogFragment.kt
@@ -7,11 +7,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.RequestManager
 import com.hefny.hady.blogpost.R
+import com.hefny.hady.blogpost.models.BlogPost
 import com.hefny.hady.blogpost.ui.main.blog.state.BlogStateEvent
+import com.hefny.hady.blogpost.util.TopSpacingItemDecoration
 import kotlinx.android.synthetic.main.fragment_blog.*
+import javax.inject.Inject
 
-class BlogFragment : BaseBlogFragment() {
+class BlogFragment : BaseBlogFragment(), BlogListAdapter.Interaction {
+    @Inject
+    lateinit var requestManager: RequestManager
+
+    private lateinit var recyclerAdapter: BlogListAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -22,9 +32,7 @@ class BlogFragment : BaseBlogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        goViewBlogFragment.setOnClickListener {
-            findNavController().navigate(R.id.action_blogFragment_to_viewBlogFragment)
-        }
+        initRecyclerView()
         subscribeObservers()
         executeSearch()
     }
@@ -49,7 +57,47 @@ class BlogFragment : BaseBlogFragment() {
             }
             viewModel.viewState.observe(viewLifecycleOwner, Observer { viewState ->
                 Log.d(TAG, "BlogFragment, viewState: $viewState")
+                if (viewState != null) {
+                    recyclerAdapter.submitList(
+                        blogList = viewState.blogFields.blogList,
+                        isQueryExhausted = true
+                    )
+                }
             })
         })
+    }
+
+    private fun initRecyclerView() {
+        blog_post_recyclerview.apply {
+            layoutManager = LinearLayoutManager(this@BlogFragment.context)
+            val topSpacingDecorator = TopSpacingItemDecoration(30)
+            removeItemDecoration(topSpacingDecorator) // does nothing if not applied already
+            addItemDecoration(topSpacingDecorator)
+
+            recyclerAdapter = BlogListAdapter(requestManager, this@BlogFragment)
+            addOnScrollListener(object : RecyclerView.OnScrollListener() {
+
+                override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                    super.onScrollStateChanged(recyclerView, newState)
+                    val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+                    val lastPosition = layoutManager.findLastVisibleItemPosition()
+                    if (lastPosition == recyclerAdapter.itemCount.minus(1)) {
+                        Log.d(TAG, "BlogFragment: attempting to load next page...")
+//                    TODO("load next page using ViewModel")
+                    }
+                }
+            })
+            adapter = recyclerAdapter
+        }
+    }
+
+    override fun onItemSelected(position: Int, item: BlogPost) {
+        Log.d(TAG, "onItemSelected: position, BlogPost: $position, ${item}")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        // clear references (can leak memory)
+        blog_post_recyclerview.adapter = null
     }
 }
---
+++ app/src/main/java/com/hefny/hady/blogpost/util/GenericViewHolder.kt
@@ -0,0 +1,9 @@
+package com.hefny.hady.blogpost.util
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class GenericViewHolder
+constructor(
+    itemView: View
+) : RecyclerView.ViewHolder(itemView)
